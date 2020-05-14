3acbc1ff8b74ab84be38b81f85100a2f9a340f50
fuerte <fuejoh4730@gmail.com>
2020-05-14 17:44:09 -0400

    404 styling-centering

--- public_html/404.php
+++ public_html/404.php
@@ -6,10 +6,10 @@
 </head>
 <body>
     <?php include("includes/header.php");?>
-    <main>
-        <h1 class="centered">404 Not Found</h1>
-        <img src="/images/404.png" class="centered" alt="Three men on a boat using sextants to navigate.">
-        <p class="centeed">The page you were looking for could not be found. Try navigating back to the <a href="/">homepage</a>.</p>
+    <main class="single">
+        <h1>404 Not Found</h1>
+        <img src="/images/404.png" class="centered twoThird" alt="Three men on a boat using sextants to navigate.">
+        <p>The page you were looking for could not be found. Try navigating back to the <a href="/">homepage</a>.</p>
     </main>
 </body>
 </html>
