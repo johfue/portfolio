96e50a652736d930a8f2bfac1f921d8dc08289fe
fuerte <fuejoh4730@gmail.com>
2020-05-14 17:30:39 -0400

    404 styling

--- public_html/404.php
+++ public_html/404.php
@@ -7,9 +7,9 @@
 <body>
     <?php include("includes/header.php");?>
     <main>
-        <h1>404 Not Found</h1>
-        <img src="/images/404.png" alt="Three men on a boat using sextants to navigate.">
-        <p>The page you were looking for could not be found. Try navigating back to the <a href="/">homepage</a>.</p>
+        <h1 class="centered">404 Not Found</h1>
+        <img src="/images/404.png" class="centered" alt="Three men on a boat using sextants to navigate.">
+        <p class="centeed">The page you were looking for could not be found. Try navigating back to the <a href="/">homepage</a>.</p>
     </main>
 </body>
 </html>
