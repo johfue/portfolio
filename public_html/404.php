39ac9e1096b0f7dc0b66b2f2e40ece94ef7712d3
fuerte <fuejoh4730@gmail.com>
2020-05-14 17:08:09 -0400

    added 404 page

---
+++ public_html/404.php
@@ -0,0 +1,15 @@
+<?php include("includes/config.php");?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <?php include("includes/headTag.php");?>
+</head>
+<body>
+    <?php include("includes/header.php");?>
+    <main>
+        <h1>404 Not Found</h1>
+        <img src="/images/404.png" alt="Three men on a boat using sextants to navigate.">
+        <p>The page you were looking for could not be found. Try navigating back to the <a href="/">homepage</a>.</p>
+    </main>
+</body>
+</html>
