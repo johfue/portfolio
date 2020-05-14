081cba933c20b87a17db4440da6224e92194ef4b
fuerte <fuejoh4730@gmail.com>
2020-05-14 19:20:27 -0400

    added 50x page

---
+++ public_html/50x.php
@@ -0,0 +1,15 @@
+<?php include("includes/config.php");?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <?php include("includes/headTag.php");?>
+</head>
+<body>
+    <?php include("includes/header.php");?>
+    <main class="single">
+        <h1>Something wen't wront!?</h1>
+        <img src="/images/404.png" class="centered twoThird" alt="Three men on a boat using sextants to navigate.">
+        <p>The page you were looking for could not be found. Try navigating back to the <a href="/">homepage</a>.</p>
+    </main>
+</body>
+</html>
