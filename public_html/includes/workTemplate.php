9a3f67ebc0e4b5b5199a3f494d4dc28cfe2d830f
fuerte <fuejoh4730@gmail.com>
2020-05-15 21:30:06 -0400

    workTemplate test

---
+++ public_html/includes/workTemplate.php
@@ -0,0 +1,19 @@
+<?php include($_SERVER['DOCUMENT_ROOT']."/includes/config.php");?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <?php include($_SERVER['DOCUMENT_ROOT']."/includes/headTag.php");?>
+</head>
+<body>
+    <?php include($_SERVER['DOCUMENT_ROOT']."/includes/header.php");?>
+    <main class="work">
+    <header class="work <?php echo $CURRENT_PAGE;?>">
+        <section>
+            <h1><?php echo $PAGE_HEADER;?></h1>
+            <p><?php echo $PAGE_DESC;?></p>
+        </section>
+    </header>
+        <?php echo $CONTENT;?>
+    </main>
+</body>
+</html>
