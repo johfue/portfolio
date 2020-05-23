e8d6fbcfb60f18be5f9a4253434db603ba9f68ff
fuerte <fuejoh4730@gmail.com>
2020-05-22 22:53:02 -0400

    debg

--- public_html/gallery.js
+++ public_html/gallery.js
@@ -6,46 +6,49 @@ function _(x) {
 
 function modalExpand(focus) {
 var modal = _("modal");
-// var modalImg = _("modalImg");
-var modalVideo = _("modalVideo");
-var modalCaption = _("modalCaption");
-    console.log(modal);
-    modalMedia = focus.parentElement.previousSibling;
-    modalCaption.innerText = focus.parentElement.innerText;
-    // truncate final amount of characters
-    if (modalMedia == "video") {
-        var source = document.createElement('source');
-        source.setAttribute('src', modalMedia.firstChild.src);
-        modalVideo.appendChild(source);
-
-    }
-    else {
-        console.log(focus.parentElement);
-        console.log(focus.parentElement.previousElementSibling.src);
-        // modalImg.src = modalMedia.src;
-    }
-    modal.syle.display = "block";
-    console.log(modal);
-
+modal.syle.display = "block";
 }
 
-// function next() {
-//     focus
+// // var modalImg = _("modalImg");
+// var modalVideo = _("modalVideo");
+// var modalCaption = _("modalCaption");
+//     console.log(modal);
+//     modalMedia = focus.parentElement.previousSibling;
+//     modalCaption.innerText = focus.parentElement.innerText;
+//     // truncate final amount of characters
+//     if (modalMedia == "video") {
+//         var source = document.createElement('source');
+//         source.setAttribute('src', modalMedia.firstChild.src);
+//         modalVideo.appendChild(source);
+
+//     }
+//     else {
+//         console.log(focus.parentElement);
+//         console.log(focus.parentElement.previousElementSibling.src);
+//         // modalImg.src = modalMedia.src;
+//     }
+//     modal.syle.display = "block";
+//     console.log(modal);
+
 // }
-// function previous() {
-//     focus
+
+// // function next() {
+// //     focus
+// // }
+// // function previous() {
+// //     focus
+// // }
+
+// // swiping should tab through
+
+// function close() {
+//     modal.style.display = "none";
 // }
 
-// swiping should tab through
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// };
 
-function close() {
-    modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
-
-// escape button should close
+// // escape button should close
