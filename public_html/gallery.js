535b1a95594ff8cc15cee106b9f71721b198f1f5
fuerte <fuejoh4730@gmail.com>
2020-05-23 12:33:53 -0400

    debg

--- public_html/gallery.js
+++ public_html/gallery.js
@@ -22,7 +22,7 @@ var modalCaption = _("modalCaption");
     else {
         console.log(focus.parentElement);
         console.log(focus.parentElement.previousElementSibling);
-        console.log(focus.parentElement.previousElementSibling.src);
+        modalImg.src = modalMedia.getAttribute('src');
     }
     console.log(modal);
     modal.style.display = "block";
