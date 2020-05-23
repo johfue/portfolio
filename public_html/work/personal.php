a632f456ed3633e37695a5651b275fd3b7a78761
fuerte <fuejoh4730@gmail.com>
2020-05-22 23:07:46 -0400

    debg

--- public_html/work/personal.php
+++ public_html/work/personal.php
@@ -16,7 +16,7 @@
     <b></b>
     <?php echo $CONTENT;?>
     </main>
-    <aside id="modal">
+    <aside style="display: none;" id="modal">
         <img id="modalImg" src="/images/blank.png">
         <video id="modalVideo"></video>
         <span id="modalCaption">Hidden text</span>
