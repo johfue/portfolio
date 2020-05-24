ab8e5557feedd6ea850ff0955803347926a39e0e
fuerte <fuejoh4730@gmail.com>
2020-05-23 21:27:31 -0400

    button typos

--- public_html/work/personal.php
+++ public_html/work/personal.php
@@ -20,9 +20,9 @@
         <img id="modalImg" src="/images/blank.png">
         <video controls id="modalVideo"></video>
         <span id="modalCaption">Hidden text</span>
-        <button class="modalBtn">id="close"></button>
-        <button class="modalBtn">id="previous" onclick="previous()"></button>
-        <button class="modalBtn">id="next" onclick="next()"></button>
+        <button class="modalBtn" id="close"></button>
+        <button class="modalBtn" id="previous"></button>
+        <button class="modalBtn" id="next"></button>
     </aside>
         <script async src="/gallery.js"></script>
 
