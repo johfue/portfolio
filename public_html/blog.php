218119596f48ebad222599fa3a0f873c43f8ae70
fuerte <fuejoh4730@gmail.com>
2020-08-24 23:15:27 -0400

    added blog

--- public_html/blog.php
+++ public_html/blog.php
@@ -6,37 +6,37 @@
 </head>
 <body>
     <?php include("includes/header.php");?>
-    <main class="single" style="text-align: left;">
-        <h1>A new normal</h1>
+    <main class="single">
+        <h1 style="text-align: left;">A new normal</h1>
 <br>
-<p>I wake up. I let my dog outside. He starts barking like a mad man as soon as he sees the neighbor dog. I bring home back inside. He takes his morning nap and I walk out the door headphones in ear.</p>
+<p style="text-align: left;">I wake up. I let my dog outside. He starts barking like a mad man as soon as he sees the neighbor dog. I bring home back inside. He takes his morning nap and I walk out the door headphones in ear.</p>
 <br>
-<p>I go on my morning run.</p>
+<p style="text-align: left;">I go on my morning run.</p>
 <br>
-<p>Around the same time MAIP VEP started, I started running in the morning. Both became central parts of my summer routine.</p>
+<p style="text-align: left;">Around the same time MAIP VEP started, I started running in the morning. Both became central parts of my summer routine.</p>
 <br>
-<p>Most weekdays I could expect to be participating in a MAIP session right at noon. By then I would’ve wrapped up my run, did my daily chores in Animal Crossing and had breakfast.</p>
+<p style="text-align: left;">Most weekdays I could expect to be participating in a MAIP session right at noon. By then I would’ve wrapped up my run, did my daily chores in Animal Crossing and had breakfast.</p>
 <br>
-<p>MAIP has helped create structure during a time when there is little of that to be found. MAIP was a lot like school. Except that the speakers were way more engaging and the content a lot more directly applicable to my career.</p>
+<p style="text-align: left;">MAIP has helped create structure during a time when there is little of that to be found. MAIP was a lot like school. Except that the speakers were way more engaging and the content a lot more directly applicable to my career.</p>
 <br>
-<p>It wasn’t just the sessions creating a new sense of normalcy but the community that formed out of them. Our one group chat spawned numerous smaller ones. One for each discipline, one for fitness, one to share job opportunities, an almost annoying number of different MAIP group chats.</p>
+<p style="text-align: left;">It wasn’t just the sessions creating a new sense of normalcy but the community that formed out of them. Our one group chat spawned numerous smaller ones. One for each discipline, one for fitness, one to share job opportunities, an almost annoying number of different MAIP group chats.</p>
 <br>
-<p>And they added so much to the experience.</p>
+<p style="text-align: left;">And they added so much to the experience.</p>
 
     </main>
     <br><br>
     <main class="single" style="text-align: left;">
         <h1>My secret</h1>
         <br>
-        <p>My secret is that I did not find out about MAIP until about a week before the deadline, and the application for art director fellows was very intensive. But I found an alternative way in, applying as a creative technologist. Because here’s the thing about me, I’m an art director who’s really into web design. And not just that I’m a web designer who can code and thinks the way HTML and CSS natively work is really, really cool. I am a front-end developer. That sounds enough like a creative technologist to me, and MAIP seemed to agree.</p>
+        <p style="text-align: left;">My secret is that I did not find out about MAIP until about a week before the deadline, and the application for art director fellows was very intensive. But I found an alternative way in, applying as a creative technologist. Because here’s the thing about me, I’m an art director who’s really into web design. And not just that I’m a web designer who can code and thinks the way HTML and CSS natively work is really, really cool. I am a front-end developer. That sounds enough like a creative technologist to me, and MAIP seemed to agree.</p>
 <br>
-<p>I would find out that I was only one of two fellows out of the entire MAIP class to be accepted as creative technologists. I was quite the unicorn.</p>
+<p style="text-align: left;">I would find out that I was only one of two fellows out of the entire MAIP class to be accepted as creative technologists. I was quite the unicorn.</p>
 <br>
-<p>The 2nd phase of the program broke us into our respective disciplines. Us two creative technologists were lumped with the UI/UX folks. Together we formed a group of less than 15. Our trainings focused on UI and UX. While not what I was expecting, were still interesting. They introduced me to the field of product design, which meshed well with philosophy towards advertising. I learned about the Kano model for prioritizing product features.</p>
+<p style="text-align: left;">The 2nd phase of the program broke us into our respective disciplines. Us two creative technologists were lumped with the UI/UX folks. Together we formed a group of less than 15. Our trainings focused on UI and UX. While not what I was expecting, were still interesting. They introduced me to the field of product design, which meshed well with philosophy towards advertising. I learned about the Kano model for prioritizing product features.</p>
 <br>
-<p>Not only that but I got introduced to some talented UX/UI designers. All of whom are super motivated and driven people. They added a lot to the experience. During every session they were asking questions I didn't even know I wanted the answer to. </p>
+<p style="text-align: left;">Not only that but I got introduced to some talented UX/UI designers. All of whom are super motivated and driven people. They added a lot to the experience. During every session they were asking questions I didn't even know I wanted the answer to. </p>
 <br>
-<p>Ultimately however through this experience, I learned that creative technology is not for me. In theory it is. The way it was sold to me is that in modern agencies on the cutting edge, the typical AD/CW team has gone the way of the dodo. Nowadays creative teams are composed of multiple teams of people of different disciplines, developers, designers, writers, are working on concepting together. I learned that is not the case, creative technologists often are not included in the concepting stage. Oftentimes they are relegated to just being producers. Which is not as exciting to me.</p>
+<p style="text-align: left;">Ultimately however through this experience, I learned that creative technology is not for me. In theory it is. The way it was sold to me is that in modern agencies on the cutting edge, the typical AD/CW team has gone the way of the dodo. Nowadays creative teams are composed of multiple teams of people of different disciplines, developers, designers, writers, are working on concepting together. I learned that is not the case, creative technologists often are not included in the concepting stage. Oftentimes they are relegated to just being producers. Which is not as exciting to me.</p>
 <br>
     </main>
 </body>
