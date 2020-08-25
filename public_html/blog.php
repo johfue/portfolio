9644daa73e69754cbe7cb23537e90479aef77524
fuerte <fuejoh4730@gmail.com>
2020-08-24 21:59:47 -0400

    added blog

--- public_html/blog.php
+++ public_html/blog.php
@@ -6,15 +6,17 @@
 </head>
 <body>
     <?php include("includes/header.php");?>
-    <main class="single">
+    <main>
+        <article class="single">
         <h1>404 Not Found</h1>
         <img src="/images/404.png" class="center twoThird" alt="Three men on a boat using sextants to navigate.">
         <p>The page you were looking for could not be found. Try navigating back to the <a href="/">homepage</a>.</p>
     </main>
-    <main class="single">
+    <article class="single">
         <h1>My secret</h1>
         <img src="/images/404.png" class="center twoThird" alt="Three men on a boat using sextants to navigate.">
         <p>The page you were looking for could not be found. Try navigating back to the <a href="/">homepage</a>.</p>
+    </article>
     </main>
 </body>
 </html>
