ec4ee96ee40e8becb5127a2ed823ae506859b48b
fuerte <fuejoh4730@gmail.com>
2021-02-02 18:28:31 -0500

    fixed broken hyperlinks

--- html/includes/config.php
+++ html/includes/config.php
@@ -299,7 +299,7 @@ switch ($_SERVER["SCRIPT_NAME"]) {
             $CURRENT_PAGE = "Play Loteria Online";
 			$PAGE_TITLE = "Loteria";
 			$PAGE_HEADER = "PlayLoteria.Online";
-			$PAGE_DESC = "<b>The Problem:</b> During the pandemic, I realized there did not exist a good online version of the mexican board game Loteria. Google made a good attempt but too much of it was automated. The funnest part, drawing the cards, was done by a robot, and even worse, the game doesn't allow you to cheat.<br><br><b>The Big Idea:</b> So I created my own version, with as much of the authentic chaos as the real thing, playable at <a href='https://playloteria.online' target='_blank'> playloteria.onlilne" . $external . "</a>";
+			$PAGE_DESC = "<b>The Problem:</b> During the pandemic, I realized there did not exist a good online version of the mexican board game Loteria. Google made a good attempt but too much of it was automated. The funnest part, drawing the cards, was done by a robot, and even worse, the game doesn't allow you to cheat.<br><br><b>The Big Idea:</b> So I created my own version, with as much of the authentic chaos as the real thing, playable at <a href='http://playloteria.online' target='_blank'> playloteria.onlilne" . $external . "</a>";
 			$CONTENT = '
                     <h2>Browser Game</h2>
                         <figure class="wide">
@@ -344,7 +344,7 @@ switch ($_SERVER["SCRIPT_NAME"]) {
                             <video autoplay loop muted playsinline>
                             <source src="/media/syrinj.mp4">
                             </video>
-                            <figcaption>Parody Website <a href="http://www.syrinj.com" target="_blank">' . $external . '</a></figcaption>
+                            <figcaption>Parody Website <a href="http://syrinj.com" target="_blank">' . $external . '</a></figcaption>
                             <button onclick="modalExpand(this)">' . $zoomIcon .'</button>
                         </figure>
                         <figure class="tall">
