36f847739a202a5a39c03c343440ea8026c794fc
fuerte <fuejoh4730@gmail.com>
2021-06-25 14:39:21 -0400

    edited about me

--- html/about.php
+++ html/about.php
@@ -16,13 +16,13 @@
     <main class="about">
         <div class="about__column about__column--one">
             <h1 class="about__h1">Chicano creative. First-time thesaurus owner. Inquisitive and gumptious autodidact.</h1>
-            <img src="/images/profPic.png" class="about__img about__img--left" alt="A rather cool guy in denim jacket standing in front of pink flowers.">
+            <img src="/images/profPic.jpg" class="about__img about__img--left" alt="A rather cool guy in denim jacket standing in front of pink flowers.">
         </div>
         <div class="about__column about__column--two">
             <section class="about__section">
                 <p>Like any child of immigrants, I share the experience of having to translate for my parents at the Burger King. Without anyone to help me with my English homework, I had to learn a lot on my own. And I grew to be a voracious self-learner. Libraries are scared to let me in their doors.<br><br>I’m a recent grad looking to be an art director. Making things people want to look at and like to use is how I get my sick kicks.</p>
             </section>
-            <img src="/images/profPic.png" class="about__img about__img--center" alt="">
+            <img src="/images/profPic.jpg" class="about__img about__img--center" alt="">
         </div>
         <div class="about__column about__column--three">
             <section class="about__section">
@@ -36,7 +36,7 @@
                 <br>
                 <a href="">Resume</a>
             </section>
-            <img src="/images/profPic.png" class="about__img about__img--right" alt="">
+            <img src="/images/profPic.jpg" class="about__img about__img--right" alt="">
         </div>
     </main>
 </body>
