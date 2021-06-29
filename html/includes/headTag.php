4967b3edeffa67001d713b43b5a730bc2a8f922a
fuerte <fuejoh4730@gmail.com>
2021-06-29 14:58:25 -0400

    added og tags

--- html/includes/headTag.php
+++ html/includes/headTag.php
@@ -11,6 +11,13 @@
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#092543">
     <meta name="msapplication-TileColor" content="#092543">
     <meta name="theme-color" content="#092543">
+    <meta property="og:site_name" content="Johnathon Fuerte" />
+    <meta property=“og:title” content="but I wasn't expecting any visitors today. Sorry for the mess." />
+    <meta property="og:description" content="It's fine really. I'm uh, an art director and uh, you know what just take a look for yourself." />
+    <meta property="og:url" content="https://johnathonfuerte.com" />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="https://johnathonfuerte.com/images/shareCard.png" />
+
 <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-140015611-1"></script>
     <script>
--- html/index.php
+++ html/index.php
@@ -8,9 +8,9 @@
 <body>
 
     <main class="grid">
-        <header class="header-home">
+        <header class="header--home">
             <img src="images/logo.svg" class="logo" alt="Logo image">
-            <section class="header-home-items">
+            <section class="header_items--home">
                 <h1>Johnathon Fuerte</h1>
                 <h2>Art Director</h2>
                 <br>
@@ -22,40 +22,40 @@
                 </nav>
             </section>
         </header>
-        <a href="work/sparknotes" class="thumbnailWrap">
+        <a href="work/sparknotes" class="thumbnail__wrap">
             <figure class="thumbnail">
                 <h3 class="zigzag">SparkNotes</h3>
-                <img src="images/sparkThumb.png" alt="Sparknotes thumbnail" class="thumbnailImage">
+                <img src="images/sparkThumb.png" alt="Sparknotes thumbnail" class="thumbnail__image">
             </figure>
         </a>
-        <a href="work/citi" class="thumbnailWrap">
+        <a href="work/citi" class="thumbnail__wrap">
             <figure class="thumbnail">
                 <h3 class="zigzag">Citi</h3>
-                <img src="images/citiThumb.png" alt="Citi thumbnail" class="thumbnailImage">
+                <img src="images/citiThumb.png" alt="Citi thumbnail" class="thumbnail__image">
             </figure>
         </a>
-        <a href="work/loteria" class="thumbnailWrap">
+        <a href="work/loteria" class="thumbnail__wrap">
             <figure class="thumbnail">
                 <h3 class="zigzag">Play Lotería Online</h3>
-                <img src="images/loteriaThumb.png" alt="Loteria thumbnail" class="thumbnailImage">
+                <img src="images/loteriaThumb.png" alt="Loteria thumbnail" class="thumbnail__image">
             </figure>
         </a>
-        <a href="work/finalstraw" class="thumbnailWrap">
+        <a href="work/finalstraw" class="thumbnail__wrap">
             <figure class="thumbnail">
                 <h3 class="zigzag">Final Straw</h3>
-                <img src="images/finalThumb.png" alt="Final Straw thumbnail" class="thumbnailImage">
+                <img src="images/finalThumb.png" alt="Final Straw thumbnail" class="thumbnail__image">
             </figure>
         </a>
-        <a href="work/jiffy" class="thumbnailWrap">
+        <a href="work/jiffy" class="thumbnail__wrap">
             <figure class="thumbnail">
                 <h3 class="zigzag">Jiffy Lube</h3>
-                <img src="images/jiffyThumb.png" alt="Jiffy Lube thumbnail" class="thumbnailImage">
+                <img src="images/jiffyThumb.png" alt="Jiffy Lube thumbnail" class="thumbnail__image">
             </figure>
         </a>
-        <a href="work/laso" class="thumbnailWrap">
+        <a href="work/laso" class="thumbnail__wrap">
             <figure class="thumbnail">
                 <h3 class="zigzag">Latin American Student Organization</h3>
-                <img src="images/lasoThumb.png" alt="LASO thumbnail" class="thumbnailImage">
+                <img src="images/lasoThumb.png" alt="LASO thumbnail" class="thumbnail__image">
             </figure>
         </a>
         <footer>
