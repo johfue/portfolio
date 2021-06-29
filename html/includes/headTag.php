5371ac196821e5b37f4361b7d00317b7158675a7
fuerte <fuejoh4730@gmail.com>
2021-06-29 15:37:19 -0400

    fixed og tags

--- html/includes/headTag.php
+++ html/includes/headTag.php
@@ -14,11 +14,11 @@
     <meta property="og:site_name" content="Johnathon Fuerte" />
     <meta property="og:title" content="but I wasn't expecting any visitors today. Sorry for the mess." />
     <meta property="og:description" content="It's fine really. I'm, uh, an art director and, uh, you know what just take a look for yourself." />
-    <meta property="og:url" content="https://test.johnathonfuerte.com" />
+    <meta property="og:url" content="https://johnathonfuerte.com" />
     <meta property="og:type" content="website" />
-    <meta property="og:image" content="https://test.johnathonfuerte.com/images/shareCard.png" />
+    <meta property="og:image" content="https://johnathonfuerte.com/images/shareCard.png" />
     <meta property="twitter:card" content="summary_large_image" />
-    <meta property="twitter:image" content="https://test.johnathonfuerte.com/images/shareCard.png" />
+    <meta property="twitter:image" content="https://johnathonfuerte.com/images/shareCard.png" />
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-140015611-1"></script>
