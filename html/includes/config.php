b60a915bc87e19ece49978ffaf76d41770a6a02d
fuerte <fuejoh4730@gmail.com>
2021-07-03 13:46:05 -0400

    fixed work btn and caption spacing issue

--- html/includes/config.php
+++ html/includes/config.php
@@ -21,7 +21,7 @@ switch ($_SERVER["SCRIPT_NAME"]) {
 			$CURRENT_PAGE = "LASO";
 			$PAGE_TITLE = "LASO";
 			$PAGE_HEADER = "Latin American Student Organization";
-			$PAGE_DESC = "<b>About:</b> As the creative director of the Latin American Student Organization at USC I was responsible for developing the organization's visual identity and creating all promotional materials. Given our how many different nationalities made up our community, it was important to me that our look pulled from all across Latin America";
+			$PAGE_DESC = "<b>About:</b> As the creative director of the Latin American Student Organization at USC I was responsible for developing the organization's visual identity and creating all promotional materials. Given our how many different nationalities made up our community, it was important to me that our look pulled from all across Latin America.";
 			$CONTENT =
 			        '
                         <h2 class=" work-h2 wide">Digital Signage</h2>
@@ -228,7 +228,7 @@ switch ($_SERVER["SCRIPT_NAME"]) {
 			$PAGE_DESC = "<b>The Problem:</b> Anxiety is on the rise among Millenials. Many are living far from their family who they used to rely on for car knowledge. With their car being one of their most important possessions, trusting a mechanic can feel like a leap of faith.<br><br><b>The Big Idea:</b> Create an app that brings as much transparency to table as possible.";
 			$CONTENT = '
                         <h2 class=" work-h2 work-h2">Mobile App</h2>
-                        <iframe class="wide center" style="border: 1px solid rgba(0, 0, 0, 0.1); height: 1000px;" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FA7VH9X3Q6MjNb29iVD34TX%2FJiffy-Lube%3Fnode-id%3D1%253A6%26scaling%3Dmin-zoom&chrome=DOCUMENTATION" allowfullscreen></iframe>
+                        <iframe class="wide center" style="border: 1px solid rgba(0, 0, 0, 0.1); height: 1000px; width: 100%;" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FA7VH9X3Q6MjNb29iVD34TX%2FJiffy-Lube%3Fnode-id%3D1%253A6%26scaling%3Dmin-zoom&chrome=DOCUMENTATION" allowfullscreen></iframe>
                         <figure class="work__figure">
                             <img src="/images/jiffyService.png" class="expandableContent center" alt=A screenshot the schedule page of the app. A grid of services are displayed with a make appointment button at the bottom.">
                             <figcaption>Before scheduling an appointment, users can review their service history and pick their mechanic based off user reviews.</figcaption>
@@ -239,24 +239,72 @@ switch ($_SERVER["SCRIPT_NAME"]) {
                             <figcaption>Users can keep tabs on all their family\'s vehicles, their mileage, and keep track of when they\'ll next need a tune up</figcaption>
                             <button class="expandBtn expandBtn-caption" onclick="modalExpand(this)">' . $zoomIcon . '</button>
                         </figure>
-                        <h2 class=" work-h2 work__figure wide">Spotify Playlist</h2>
+                        <h2 class=" work-h2 wide">Spotify Playlist</h2>
                         <p>Jiggy with Jiffy provides music to soothe your mind while you drive, or while you wait.<br>The Spotify playlist can be accessed through the Jiffy Lube app via “Browse.”</p>
                         <figure class="work__figure tall">
                             <img src="/images/jiffyPlaylist.png" class="expandableContent center" alt=A screenshot of a Spotify playlist.">
                             <button class="expandBtn" onclick="modalExpand(this)">' . $zoomIcon . '</button>
                         </figure>
-                        <h2 class=" work-h2 work__figure wide">Full Deck</h2>
+                        <h2 class=" work-h2 wide">Full Deck</h2>
                         <iframe src="//www.slideshare.net/slideshow/embed_code/key/GoMWXY4mqs875c" class="center" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe>
                         <p class="wide center">Role / Creative Technologist<br><br>AD / Sydia Fraguada<br>AD / Andres Urdaneta<br>Designer / Kurt Kyle Juanillo<br>CW / Tamara Vasquez<br>Media / Barron Coleman<br>Media / Jessica Smith<br> Strategist / Micha Lewis<br>Strategist / Dallas Watkins</p>
                         ';
 			break;
+		case "/work/jiffy2021.php":
+			$CURRENT_PAGE = "Jiffy Lube";
+			$PAGE_TITLE = "Jiffy Lube";
+			$PAGE_HEADER = "Jiffy Lube";
+			$PAGE_DESC = "<b>The Problem:</b> Car ownership is important but car care is complicated and trustworthy mechanics are hard to come by.<br><br><b>The Big Idea:</b> When people move to a new city they lose whoever they did trust to service their car. Show them Jiffy mechanics go the extra mile for their drivers.";
+			$CONTENT = '
+                        <h2 class="work-h2 wide">Bumble BFF</h2>
+                        <p>In the top cities people are moving to, Bumble profiles with personalities based on local Jiffy Lube technicians will recommend their matches places to visit and help people get settled into their new city. Like any good mechanic, they wouldn’t recommend something they wouldn’t do themself.</p>
+                        <figure class="work__figure">
+                            <img src="/images/jiffyBumble.jpg" class="expandableContent center" alt="A screenshot of a mock Bumble profile. The profiles name is Avery.">
+                            <button class="expandBtn expandBtn-caption" onclick="modalExpand(this)">' . $zoomIcon . '</button>
+                        </figure>
+                        <h2 class="work-h2 wide">Direct Mail</h2>
+                        <p>People who’ve just moved to a target city will receive a welcome package. Included is a living guide with info about local restaurants, fun destinations, and where to find essential services. As well as information on parking, street cleanings, traffic patterns, and how to update their drivers license. Along with the guide, the box will include items to help them through an anxious time:<ul><li>Essential oil aromatic air freshener</li><li>QR code with relaxing driving spotify playlist</li><li>Fidget toy Keychain</li><li>Set of postcards to send to their friends back home</li></ul></p>
+                        <figure class="work__figure">
+                            <img src="/images/jiffyBox.jpg" class="expandableContent center" alt="">
+                            <button class="expandBtn expandBtn-caption" onclick="modalExpand(this)">' . $zoomIcon . '</button>
+                        </figure>
+                        <h2 class="work-h2 wide">Mobile App</h2>
+                        <p>Along with a visual refresh, the Jiffy Lube app will get a few new updates.<ul><li>Since cars need maintenance for years, users can get to know who will be handling their service trip ahead of time to build long-term trust.<li>If something urgent pop-ups up users can chat in real-time with a Jiffy Lube technician to diagnose their problem.<li>The app will keep track of every service visit so users can track how often they need maintenance and who all has worked on their car.</ul></p>
+                        <figure class="work__figure">
+                            <iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FPHEdQBLSEGZ56w7yj0qANK%2FJiffy-Lube-2021" allowfullscreen></iframe>
+                            <button class="expandBtn" onclick="modalExpand(this)">' . $zoomIcon . '</button>
+                            <figcaption>Interactive Demo</figcaption>
+                        </figure>
+                        <figure class="work__figure">
+                            <img src="/images/jiffyApp.jpg" class="expandableContent center" alt=">
+                            <figcaption>The new appointment confirmation process.</figcaption>
+                            <button class="expandBtn" onclick="modalExpand(this)">' . $zoomIcon . '</button>
+                        </figure>
+                        <h2 class="work-h2 wide">Out-of-home</h2>
+                        <p>In target cities Jiffy Lube will give pointers to newcomers through billboards. The goal is to associate Jiffy Lube with solid automotive advice. So whenever someone uses what they’ve learned from a billboard they\'ll remember they learned it from Jiffy Lube.</p>
+                        <figure class="work__figure">
+                            <img src="/images/jiffyOOH_1.jpg" class="expandableContent center" alt="The copy reads: Welcome to Denver. The tallest dunes in North America are an afternoon away. Happy driving. Jiffy Lube.">
+                            <button class="expandBtn expandBtn-caption" onclick="modalExpand(this)">' . $zoomIcon . '</button>
+                        </figure>
+                        <figure class="work__figure">
+                            <img src="/images/jiffyOOH_2.jpg" class="expandableContent center" alt="The copy reads: Welcome to Pittsburg. Avoid driving through North Shore on Game Days. Happy driving. Jiffy Lube.">
+                            <button class="expandBtn expandBtn-caption" onclick="modalExpand(this)">' . $zoomIcon . '</button>
+                        </figure>
+                        <figure class="work__figure">
+                            <img src="/images/jiffyOOH_3.jpg" class="expandableContent center" alt="The copy reads: Welcome to Durham. Meters are free on the weekend. Happy driving. Jiffy Lube.">
+                            <button class="expandBtn expandBtn-caption" onclick="modalExpand(this)">' . $zoomIcon . '</button>
+                        </figure>
+                        <p class="wide center">Role / Creative Technologist<br><br>AD / Sydia Fraguada<br>AD / Andres Urdaneta<br>Designer / Kurt Kyle Juanillo<br>CW / Tamara Vasquez<br>Media / Barron Coleman<br>Media / Jessica Smith<br> Strategist / Micha Lewis<br>Strategist / Dallas Watkins</p>
+                        ';
+			break;
+
 		case "/work/citi.php":
 			$CURRENT_PAGE = "Citi";
 			$PAGE_TITLE = "Citi";
 			$PAGE_HEADER = "Citi";
 			$PAGE_DESC = "<b>The Problem:</b> Citi wants to demonstrate their commitment to driving an equitable recovery for working women following the pandemic.<br><br><b>The Big Idea:</b> Have Citi connect working women and give them the tools to succeed financially, because knowledge is power and community builds confidence.";
 			$CONTENT = '
-                        <h2 class=" work-h2 wide">Women\'s Financial League</h2>
+                        <h2 class="work-h2 wide">Women\'s Financial League</h2>
                         <p>The WFL is a learning community that uses gamification to teach financial literacy. Users will join one of two teams and compete against each other to achieve weekly goals. Megan Rapinoe of the US Women\'s Soccer Team will serve as spokesperson for the league.</p>
                         <figure class="work__figure">
                             <img src="/images/citiWFL.jpg" class="expandableContent center" alt="A graphic featuring two logos with a versus symbol in between them. One is of a roman style helmet glowing bright orange and the other is a of a valkyrie helmet glowing neon green. They are surrounded by swirling energy against an electric blue background.">
