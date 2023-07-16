13edd5a3d29edbd51b5b5e907d107fe2f9b93d00
fuerte <fuejoh4730@gmail.com>
2023-07-16 15:15:28 -0500

    deleted old files

--- html/includes/header.php
+++
@@ -1,9 +0,0 @@
-    <nav class="nav--small">
-        <a href="/" title="Home" class="nav__logo--small"><?php echo file_get_contents($_SERVER['DOCUMENT_ROOT']."/images/logo.svg"); ?></a>
-        <ul class="nav__ul--small">
-            <li class="nav__li--small"><a href="/about" class="nav__a">About Me</a></li>
-            <li class="nav__li--small"><a href="/work/personal" class="nav__a">Fun</a></li>
-        </ul>
-    </nav>
-    <b class="spacer spacer-small"></b>
-    <b class="spacer spacer-small"></b>
