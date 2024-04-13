ed0835e2ce1473df11be855db1a1ef340a96638e
fuejoh <fuejoh4730@gmail.com>
2024-04-13 11:01:42 -0500

    redfined layout tags and terminix layout

--- html/pages/work/terminix.js
+++ html/pages/work/terminix.js
@@ -29,41 +29,40 @@ export function Content() {
     <>
 
     <section class="work__section work__section--main ">
-        <div class="work__div">
+        <div class="work__div col-mid">
             <h2 class="work-h2">Mnemonic</h2>
-            <p>I designed a flexible new mnemonic that tells the story of the brand in two seconds.</p>
+            <p class="work__p">I designed a flexible new mnemonic that tells the story of the brand in two seconds.</p>
         </div>
-        <Media type="video" src="/media/terminixEndTag.mp4" class="col-3 center" autoplay="true"/>
+        <Media type="video" src="/media/terminixEndTag.mp4" class="col-3" autoplay="true"/>
     </section>
     
     <section class="work__section work__section--accent light">
-    <div class="work__decor" id="work__decor--terminix"></div>
-
-        <h2 class="work-h2 col-right">:15s Spots</h2>
-        <Media type="video" src="/media/terminixHornetNest.mp4" class="col-2 col-right" unexpandable/>
-        <Media type="video" src="/media/terminixRoachArt.mp4" class="col-2 col-right" unexpandable/>
-        <Media type="video" src="/media/terminixUnmounted.mp4" class="col-2 col-right" unexpandable/>
+        <div class="work__decor" id="work__decor--terminix"></div>
+        <h2 class="work-h2">:15s Spots</h2>
+        <Media type="video" src="/media/terminixHornetNest.mp4" class="col-2 col-mid" unexpandable/>
+        <Media type="video" src="/media/terminixRoachArt.mp4" class="col-2 col-mid" unexpandable/>
+        <Media type="video" src="/media/terminixUnmounted.mp4" class="col-2 col-mid" unexpandable/>
     </section>
     
     <section class="work__section work__section--main">
         <span></span>
-        <span></span>
         <div class="work__div">
             <h2 class="work-h2">:06s Animations</h2>
-            <p>Fit for any short form space. Perfect for reinforcing the new branding.</p>
+            <p class="work__p">Fit for any short form space. Perfect for reinforcing the new branding.</p>
         </div>
+        <span></span>
         <Media type="video" src="/media/terminixSpider.mp4" unexpandable/>
         <Media type="video" src="/media/terminixRoach.mp4" unexpandable/>
         <Media type="video" src="/media/terminixTermite.mp4" unexpandable/>
     </section>
     
     <section class="work__section work__section--accent light">
-        <div class="work__div">
+        <div class="work__div col-right bottom">
         <h2 class="work-h2">Social</h2>
-        <p class="col-right">To help our cut downs feel a little more native, we took a cue from what's hip with the youth these days.</p>
+        <p class="work__p">To help our cut downs feel a little more native, we took a cue from what's hip with the youth these days.</p>
         </div>
-        <div class="pillarBox">
-        <Media type="video" src="/media/terminixFancam.mp4" unexpandable short/>
+        <div class="pillarBox col-mid">
+        <Media type="video" src="/media/terminixFancam.mp4" unexpandable short />
         </div>
         <div class="pillarBox">
         <Media type="video" src="/media/terminixRoachesEverywhere.mp4"unexpandable short/>
@@ -71,11 +70,10 @@ export function Content() {
     </section>
     <section class="work__section work__section--main">
         <span></span>
-        <span></span>
         <h2 class="work-h2">Out-of-Home</h2>
-        <Media type="image" src="/images/terminix_OOH_CHI.jpg" class="col-2 col-right" alt="The copy reads: Welcome to Durham. Meters are free on the weekend. Happy driving. Jiffy Lube."/>
-        <div class="work__div col-3">
-            <p class="center">
+        <Media type="image" src="/images/terminix_OOH_CHI.jpg" class="col-2 col-mid" alt="The copy reads: Welcome to Durham. Meters are free on the weekend. Happy driving. Jiffy Lube."/>
+        <div class="work__div col-3 right">
+            <p class="text-right">
                 ECD - Amy Ditchman<br></br>
                 CD - Adam Repp<br></br>
                 ACD - Deidre Nieto<br></br>
