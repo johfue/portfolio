f247d081cc397dc0a339f4545dd13e046d95b9d2
fuejoh <fuejoh4730@gmail.com>
2026-03-02 20:55:41 -0600

    More Copy tweaks

--- html/pages/work/jiffy.js
+++ html/pages/work/jiffy.js
@@ -56,7 +56,7 @@ export function Content() {
             </div>
         </div>
         <figure class="work__figure center col-2">
-            <iframe style={{border: '1px solid rgba(0, 0, 0, 0.1)', width: '51vw', 'min-width': '100%', height: '600px'}} src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FPHEdQBLSEGZ56w7yj0qANK%2FJiffy-Lube-2021%3Fnode-id%3D1%253A6%26scaling%3Dscale-down%26page-id%3D0%253A1" allowfullscreen></iframe>
+            <iframe style={{border: '1px solid rgba(0, 0, 0, 0.1)', width: '51vw', 'min-width': '100%', height: '600px','max-width':'1000px'}} src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FPHEdQBLSEGZ56w7yj0qANK%2FJiffy-Lube-2021%3Fnode-id%3D1%253A6%26scaling%3Dscale-down%26page-id%3D0%253A1" allowfullscreen></iframe>
 
             <figcaption>Interactive Demo</figcaption>
         </figure>
